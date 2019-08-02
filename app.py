18c882e46b09712ea1d50efa193c3eff9acafa49
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-08-02 17:06:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,61 @@ client = commands.Bot(command_prefix='-')
 #create an arraylist containing phrases you want your bot to switch through.
 status = cycle(['DM BlackRabbit for advertise in DMs'])
 
+@client.event
+async def on_message(message):
+    message.content = message.content.lower()
+    author = '{0.author.mention}'.format(message)
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('hello'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+       
+    if message.content.startswith('hi'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+       
+    if message.content.startswith('Yo'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+       
+    if message.content.startswith('Ye'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+       
+    if message.content.startswith('Good morning'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+             
+    if message.content.startswith('good evening'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+              
+    if message.content.startswith('Rabbit'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg
+                                  
+    if message.content.startswith('please'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+                                                                 
+    if message.content.startswith('advertise'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+                                  
+    if message.content.startswith('are you there'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+                                                                                                    
+    if message.content.startswith('are you here'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
+                                                                    
+    if message.content.startswith('eshop'):
+        msg = '**DM to BlackRabbit001#3981 {0.author.mention}'.format(message)
+        await message.author.send(msg)
 
    
 @client.event
