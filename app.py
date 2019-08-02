5f56910cfc11e02ca0af3ed316b0e3449dbf09b0
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-08-02 17:25:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix='!')
 #client = discord.Client()
 
 #create an arraylist containing phrases you want your bot to switch through.
-status = cycle(['www.rabbit001.cf', 'With BlackRabbit', 'with Generator', 'with accounts'])
+status = cycle(['DM for advertise in DMs', 'DM for advertise in DMs', 'DM for advertise in DMs', 'DM for advertise in DMs'])
 
 @client.command()
 async def lala(ctx):
